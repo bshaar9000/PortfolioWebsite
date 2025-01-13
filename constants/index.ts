4d64c198848ea68b15c85ea86acee214a7924f9c
bshaar9000 <72175671+bshaar9000@users.noreply.github.com>
2025-01-13 13:14:47 -0500

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -56,20 +56,6 @@ export const SkillData = [
   
 ];
 
-export const Socials = [
-  {
-    name: "Discord",
-    src: "/instagram.svg",
-  },
-  {
-    name: "Facebook",
-    src: "/facebook.svg",
-  },
-  {
-    name: "Instagram",
-    src: "/discord.svg",
-  },
-];
 export const Projects = [
   {
     title: "Modern Nextjs Website",
